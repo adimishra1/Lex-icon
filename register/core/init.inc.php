518977813a5f12a4f5f093ffcf4ab1540c4415ad
DEEPESH <deepeshsunny8@gmail.com>
2018-06-21 15:58:54 +0530

    Added register page and main page

---
+++ register/core/init.inc.php
@@ -0,0 +1,19 @@
+<?php
+	session_start();
+	
+	$exception =  array('register', 'login');
+	//$page = substr(end(explode('/',$_SERVER['SCRIPT_NAME'])), 0, -4);
+
+	//if(in_array($page, $exception) === false){
+		// if(isset($_SESSION['username']) === false){
+		// 	header('Location: login.php');
+		// 	die();
+		// }
+	//}
+	$conn = mysqli_connect('127.0.0.1','root','21522042003','Lex') or die("Failed to query database");
+
+	$path = dirname(__FILE__);
+
+	include($path."/inc/user.inc.php");
+
+?>
