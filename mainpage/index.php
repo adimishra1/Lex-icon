c5bfad1f3d064d8b5ae4e25036df509f616ccf1c
Aditya Mishra <adimishragithub@gmail.com>
2018-07-04 22:15:25 +0530

    Removed unnecessary CSS and added loader

--- mainpage/index.php
+++ mainpage/index.php
@@ -58,9 +58,12 @@
 <link href='https://fonts.googleapis.com/css?family=Patrick+Hand+SC' rel='stylesheet' type='text/css'>
 <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.5.0/css/font-awesome.min.css" />
 <link rel="stylesheet" type="text/css" href="css/icons.css" />
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+<link href="css/Loader.css" rel="stylesheet">
+
 <style>
-/* $hoverEasing: cubic-bezier(0.23, 1, 0.32, 1);
-$returnEasing: cubic-bezier(0.445, 0.05, 0.55, 0.95); */
 
 body {
   margin: 40px 0;
@@ -78,19 +81,6 @@ body {
   text-align: center;
 }
 
-
-/*  html,body{
-  height:100%;
-}
-body{
-  text-align:center;
-}
-body:before{
-  content:'';
-  height:100%;
-  display:inline-block;
-  vertical-align:middle;
-}  */
 .fo{
 	text-align:center;
 
@@ -135,139 +125,7 @@ body:before{
 
 
 
-
-/*
-p {
-  line-height: 1.5em;
-}
-
-h1+p, p+p {
-  margin-top: 10px;
-}
-
-.container {
-  padding: 40px 80px;
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-}
-
-.card-wrap {
-  margin: 10px;
-  transform: perspective(8900px);
-  transform-style: preserve-3d;
-  cursor: pointer;
-  // background-color: #fff;
-
-  &:hover {
-    .card-info {
-      transform: translateY(0);
-    }
-    .card-info p {
-      opacity: 1;
-    }
-    .card-info, .card-info p {
-      transition: 0.6s $hoverEasing;
-    }
-    .card-info:after {
-      transition: 5s $hoverEasing;
-      opacity: 1;
-      transform: translateY(0);
-    }
-    .card-bg {
-      transition:
-        0.6s $hoverEasing,
-        opacity 5s $hoverEasing;
-      opacity: 0.8;
-    }
-    .card {
-      transition:
-        0.6s $hoverEasing,
-        box-shadow 2s $hoverEasing;
-      box-shadow:
-        rgba(white, 0.2) 0 0 40px 5px,
-        rgba(white, 1) 0 0 0 1px,
-        rgba(black, 0.66) 0 30px 60px 0,
-        inset #333 0 0 0 5px,
-        inset white 0 0 0 6px;
-    }
-  }
-}
-
-.card {
-  position: relative;
-  flex: 0 0 240px;
-  width: 240px;
-  height: 320px;
-  background-color: #333;
-  overflow: hidden;
-  border-radius: 10px;
-  box-shadow:
-    rgba(black, 0.66) 0 30px 60px 0,
-    inset #333 0 0 0 5px,
-    inset rgba(white, 0.5) 0 0 0 6px;
-  transition: 1s $returnEasing;
-}
-.card-bg {
-  opacity: 0.8;
-  position: absolute;
-  top: -20px; left: -20px;
-  width: 120%;
-  height: 120%;
-  padding: 20px;
-  background-repeat: no-repeat;
-  background-position: center;
-  background-size: cover;
-  transition:
-    1s $returnEasing,
-    opacity 5s 1s $returnEasing;
-  pointer-events: none;
-}
-
-.card-info {
-  padding: 20px;
-  position: absolute;
-  bottom: 0;
-  color: #fff;
-  transform: translateY(40%);
-  transition: 0.6s 1.6s cubic-bezier(0.215, 0.61, 0.355, 1);
-
-  p {
-    opacity: 0;
-    text-shadow: rgba(black, 1) 0 2px 3px;
-    transition: 0.6s 1.6s cubic-bezier(0.215, 0.61, 0.355, 1);
-  }
-
-  * {
-    position: relative;
-    z-index: 1;
-  }
-
-   	&:after {
-    content: '';
-    position: absolute;
-    top: 0; left: 0;
-    z-index: 0;
-    width: 100%;
-    height: 100%;
-    background-image: linear-gradient(to bottom, transparent 0%, rgba(#000, 0.6) 100%);
-    background-blend-mode: overlay;
-    opacity: 0;
-    transform: translateY(100%);
-    transition: 6s 1s $returnEasing;
-  }
-}
-
-.card-info h1 {
-  font-family: "Playfair Display";
-  font-size: 36px;
-  font-weight: 700;
-  text-shadow: rgba(black, 0.5) 0 10px 10px;
-} */
-
 .zoom {
-    /* padding: 50px; */
-    /* background-color: green; */
     transition: transform .2s; /* Animation */
     width: 200px;
     height: 200px;
@@ -638,15 +496,16 @@ h1+p, p+p {
 												$sentence = $word2['sentence'];
 												echo '<div class="row">
 																<div class="col-lg-12 col-md-4 col-sm-6 col-xs-12">
-											    <div class="hovereffect zoom">
-											        <img class="img-responsive" src="'.$link.'" alt="">
-											        <div class="overlay">
-											           <h2>'.$word.'</h2>
-											           <a class="info" href="#">'.$meaning.'<br/>
-																'.$sentence.'</a>
-										         </div>
-							</div></div>
-							</div><br /><br /><br /><br />';
+											    				<div class="hovereffect zoom">
+											        			<img class="img-responsive" src="'.$link.'" alt="">
+											        			<div class="overlay">
+											           			<h2>'.$word.'</h2>
+											           			<a class="info" href="#">'.$meaning.'<br/>
+																			'.$sentence.'</a>
+										         				</div>
+																	</div>
+																</div>
+															</div>';
 						}
 						?>
 					</div>
