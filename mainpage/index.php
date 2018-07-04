7bff8525ac4589ff155bb411fd824e5f73f00aa5
Aditya Mishra <adimishragithub@gmail.com>
2018-07-04 16:09:04 +0530

    just linked some files

--- mainpage/index.php
+++ mainpage/index.php
@@ -332,7 +332,7 @@ h1+p, p+p {
 <div id="vs-container" class="vs-container">
 
 	<div class="codrops-top clearfix">
-		<span class="right"><a href="#section-5">About</a><a href="#section-4">Favourites</a><a href="logout.php"><span>Logout</span></a></span>
+		<span class="right"><a href="aboutus/index.html">About</a><a href="#section-4">Favourites</a><a href="logout.php"><span>Logout</span></a></span>
 	</div>
 
 	<header class="vs-header">
