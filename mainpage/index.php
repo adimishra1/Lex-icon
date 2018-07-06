8cfdb208e8b07840bbf7d016175e67de48715bc2
Aakash <aakashkhandelwal3021@gmail.com>
2018-07-06 02:01:56 +0000

    video in background

--- mainpage/index.php
+++ mainpage/index.php
@@ -248,8 +248,8 @@ body {
     position: fixed;
     right: 0;
     bottom: 0;
-    min-width: 100vh;
-    min-height: 100vh;
+    min-width: 100%;
+    min-height: 100%;
 }
 
 
@@ -261,7 +261,7 @@ body {
 
 
 	<video autoplay muted loop id="myVideo">
-  <source src="your_video_name.mp4" type="video/mp4">
+  <source src="1.mp4" type="video/mp4">
 </video>
 
 
