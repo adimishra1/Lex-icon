19047184b00b8d300dc02271c622e4c60b316c9f
Aditya Mishra <adimishragithub@gmail.com>
2018-07-08 00:44:13 +0530

    Changed the Database Server

--- register2/core/inc/cred.inc.php
+++ register2/core/inc/cred.inc.php
@@ -1,7 +1,7 @@
 <?php
 $conn = mysqli_init();
 mysqli_ssl_set($conn,NULL,NULL, "BaltimoreCyberTrustRoot.crt.pem", NULL, NULL) ;
-mysqli_real_connect($conn, 'lexiconserver.mysql.database.azure.com', 'adimis@lexiconserver', 'Lexicon123', 'Lex', 3306, MYSQLI_CLIENT_SSL, MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT);
+mysqli_real_connect($conn, 'lexiconserver1.mysql.database.azure.com', 'adimis@lexiconserver1', 'Lexicon123', 'Lex', 3306, MYSQLI_CLIENT_SSL, MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT);
 if (mysqli_connect_errno($conn)) {
   die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
