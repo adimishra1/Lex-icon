13ee4935a35cd86991ddbde9b13e393f5ad10810
Aakash <aakashkhandelwal3021@gmail.com>
2018-07-07 22:13:50 +0000

    deleted previous register page

--- register/core/inc/cred.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-$conn = mysqli_init();
-mysqli_ssl_set($conn,NULL,NULL, "BaltimoreCyberTrustRoot.crt.pem", NULL, NULL) ;
-mysqli_real_connect($conn, 'lexiconserver.mysql.database.azure.com', 'adimis@lexiconserver', 'Lexicon123', 'Lex', 3306, MYSQLI_CLIENT_SSL, MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT);
-if (mysqli_connect_errno($conn)) {
-  die('Failed to connect to MySQL: '.mysqli_connect_error());
-}
-?>
