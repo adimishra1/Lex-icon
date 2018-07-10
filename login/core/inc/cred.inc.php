cce50e6b66a69c5e2063eb2c19472007712b7b97
Ritesh kumar <riteshkumartbz@gmail.com>
2018-07-10 13:28:01 +0530

    Restored the connection

--- login/core/inc/cred.inc.php
+++ login/core/inc/cred.inc.php
@@ -1,3 +1,8 @@
 <?php
-$conn = mysqli_connect('localhost','root','21522042003','Lex');
+$conn = mysqli_init();
+mysqli_ssl_set($conn,NULL,NULL, "BaltimoreCyberTrustRoot.crt.pem", NULL, NULL) ;
+mysqli_real_connect($conn, 'lexiconserver1.mysql.database.azure.com', 'adimis@lexiconserver1', 'Lexicon123', 'Lex', 3306, MYSQLI_CLIENT_SSL, MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT);
+if (mysqli_connect_errno($conn)) {
+die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
 ?>
